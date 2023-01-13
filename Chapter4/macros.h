ee37cf761781d1fd0ba49d1025f1b58c6b86d865
Sourav Ghosh <sghoshusc@gmail.com>
2023-01-13 10:07:43 -0600

    changes to match chapter text.

--- Chapter4/macros.h
+++ Chapter4/macros.h
@@ -6,14 +6,14 @@
 #define LIKELY(x) __builtin_expect(!!(x), 1)
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
 
-inline void ASSERT(bool cond, const std::string& msg) {
+inline auto ASSERT(bool cond, const std::string& msg) noexcept {
   if(UNLIKELY(!cond)) {
     std::cerr << msg << std::endl;
     exit(EXIT_FAILURE);
   }
 }
 
-inline void FATAL(const std::string& msg) {
+inline auto FATAL(const std::string& msg) noexcept {
   std::cerr << msg << std::endl;
   exit(EXIT_FAILURE);
 }
