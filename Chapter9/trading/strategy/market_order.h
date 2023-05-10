c98a4b6da8d37142ec263f194fe799a925fda993
Sourav Ghosh <sghoshusc@gmail.com>
2023-05-10 11:03:38 -0500

    minor move of where BBO is defined.

--- Chapter9/trading/strategy/market_order.h
+++ Chapter9/trading/strategy/market_order.h
@@ -56,4 +56,20 @@ namespace Trading {
   };
 
   typedef std::array<MarketOrdersAtPrice *, ME_MAX_PRICE_LEVELS> OrdersAtPriceHashMap;
+
+  struct BBO {
+    Price bid_price_ = Price_INVALID, ask_price_ = Price_INVALID;
+    Qty bid_qty_ = Qty_INVALID, ask_qty_ = Qty_INVALID;
+
+    auto toString() const {
+      std::stringstream ss;
+      ss << "BBO{"
+         << qtyToString(bid_qty_) << "@" << priceToString(bid_price_)
+         << "X"
+         << priceToString(ask_price_) << "@" << qtyToString(ask_qty_)
+         << "}";
+
+      return ss.str();
+    };
+  };
 }
--- Chapter9/trading/strategy/market_order_book.h
+++ Chapter9/trading/strategy/market_order_book.h
@@ -10,22 +10,6 @@
 namespace Trading {
   class TradeEngine;
 
-  struct BBO {
-    Price bid_price_ = Price_INVALID, ask_price_ = Price_INVALID;
-    Qty bid_qty_ = Qty_INVALID, ask_qty_ = Qty_INVALID;
-
-    auto toString() const {
-      std::stringstream ss;
-      ss << "BBO{"
-         << qtyToString(bid_qty_) << "@" << priceToString(bid_price_)
-         << "X"
-         << priceToString(ask_price_) << "@" << qtyToString(ask_qty_)
-         << "}";
-
-      return ss.str();
-    };
-  };
-
   class MarketOrderBook final {
   public:
     MarketOrderBook(TickerId ticker_id, Logger *logger);
