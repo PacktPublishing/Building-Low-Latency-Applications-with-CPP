7a1cf95e1531553db182093760fad19f8cc8282d
Sourav Ghosh <sghoshusc@gmail.com>
2023-06-17 08:38:16 -0500

    added a benchmark for std::array vs. std::unordered_map.

---
+++ Chapter12/benchmarks/hash_benchmark.cpp
@@ -0,0 +1,76 @@
+#include "matcher/matching_engine.h"
+#include "matcher/unordered_map_me_order_book.h"
+
+static constexpr size_t loop_count = 100000;
+
+template<typename T>
+size_t benchmarkHashMap(T *order_book, const std::vector<Exchange::MEClientRequest>& client_requests) {
+  size_t total_rdtsc = 0;
+
+  for (size_t i = 0; i < loop_count; ++i) {
+    const auto& client_request = client_requests[i];
+    switch (client_request.type_) {
+      case Exchange::ClientRequestType::NEW: {
+        const auto start = Common::rdtsc();
+        order_book->add(client_request.client_id_, client_request.order_id_, client_request.ticker_id_,
+                        client_request.side_, client_request.price_, client_request.qty_);
+        total_rdtsc += (Common::rdtsc() - start);
+      }
+        break;
+
+      case Exchange::ClientRequestType::CANCEL: {
+        const auto start = Common::rdtsc();
+        order_book->cancel(client_request.client_id_, client_request.order_id_, client_request.ticker_id_);
+        total_rdtsc += (Common::rdtsc() - start);
+      }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return (total_rdtsc / (loop_count * 2));
+}
+
+int main(int, char **) {
+  srand(0);
+
+  Common::Logger logger("hash_benchmark.log");
+  Exchange::ClientRequestLFQueue client_requests(ME_MAX_CLIENT_UPDATES);
+  Exchange::ClientResponseLFQueue client_responses(ME_MAX_CLIENT_UPDATES);
+  Exchange::MEMarketUpdateLFQueue market_updates(ME_MAX_MARKET_UPDATES);
+  auto matching_engine = new Exchange::MatchingEngine(&client_requests, &client_responses, &market_updates);
+
+  Common::OrderId order_id = 1000;
+  std::vector<Exchange::MEClientRequest> client_requests_vec;
+  Price base_price = (rand() % 100) + 100;
+  while (client_requests_vec.size() < loop_count) {
+    const Price price = base_price + (rand() % 10) + 1;
+    const Qty qty = 1 + (rand() % 100) + 1;
+    const Side side = (rand() % 2 ? Common::Side::BUY : Common::Side::SELL);
+
+    Exchange::MEClientRequest new_request{Exchange::ClientRequestType::NEW, 0, 0, order_id++, side, price, qty};
+    client_requests_vec.push_back(new_request);
+
+    const auto cxl_index = rand() % client_requests_vec.size();
+    auto cxl_request = client_requests_vec[cxl_index];
+    cxl_request.type_ = Exchange::ClientRequestType::CANCEL;
+
+    client_requests_vec.push_back(cxl_request);
+  }
+
+  {
+    auto me_order_book = new Exchange::MEOrderBook(0, &logger, matching_engine);
+    const auto cycles = benchmarkHashMap(me_order_book, client_requests_vec);
+    std::cout << "Array HashMap" << " " << cycles << " clock cycles." << std::endl;
+  }
+
+  {
+    auto me_order_book = new Exchange::UnorderedMapMEOrderBook(0, &logger, matching_engine);
+    const auto cycles = benchmarkHashMap(me_order_book, client_requests_vec);
+    std::cout << "UnorderedMap HashMap" << " " << cycles << " clock cycles." << std::endl;
+  }
+
+  exit(EXIT_SUCCESS);
+}
---
+++ Chapter12/exchange/matcher/unordered_map_me_order_book.cpp
@@ -0,0 +1,211 @@
+#include "unordered_map_me_order_book.h"
+
+#include "matcher/matching_engine.h"
+
+namespace Exchange {
+  UnorderedMapMEOrderBook::UnorderedMapMEOrderBook(TickerId ticker_id, Logger *logger, MatchingEngine *matching_engine)
+      : ticker_id_(ticker_id), matching_engine_(matching_engine), orders_at_price_pool_(ME_MAX_PRICE_LEVELS), order_pool_(ME_MAX_ORDER_IDS),
+        logger_(logger) {
+  }
+
+  UnorderedMapMEOrderBook::~UnorderedMapMEOrderBook() {
+    logger_->log("%:% %() % OrderBook\n%\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_),
+                toString(false, true));
+
+    matching_engine_ = nullptr;
+    bids_by_price_ = asks_by_price_ = nullptr;
+  }
+
+  /// Match a new aggressive order with the provided parameters against a passive order held in the bid_itr object and generate client responses and market updates for the match.
+  /// It will update the passive order (bid_itr) based on the match and possibly remove it if fully matched.
+  /// It will return remaining quantity on the aggressive order in the leaves_qty parameter.
+  auto UnorderedMapMEOrderBook::match(TickerId ticker_id, ClientId client_id, Side side, OrderId client_order_id, OrderId new_market_order_id, MEOrder* itr, Qty* leaves_qty) noexcept {
+    const auto order = itr;
+    const auto order_qty = order->qty_;
+    const auto fill_qty = std::min(*leaves_qty, order_qty);
+
+    *leaves_qty -= fill_qty;
+    order->qty_ -= fill_qty;
+
+    client_response_ = {ClientResponseType::FILLED, client_id, ticker_id, client_order_id,
+                        new_market_order_id, side, itr->price_, fill_qty, *leaves_qty};
+    matching_engine_->sendClientResponse(&client_response_);
+
+    client_response_ = {ClientResponseType::FILLED, order->client_id_, ticker_id, order->client_order_id_,
+                        order->market_order_id_, order->side_, itr->price_, fill_qty, order->qty_};
+    matching_engine_->sendClientResponse(&client_response_);
+
+    market_update_ = {MarketUpdateType::TRADE, OrderId_INVALID, ticker_id, side, itr->price_, fill_qty, Priority_INVALID};
+    matching_engine_->sendMarketUpdate(&market_update_);
+
+    if (!order->qty_) {
+      market_update_ = {MarketUpdateType::CANCEL, order->market_order_id_, ticker_id, order->side_,
+                        order->price_, order_qty, Priority_INVALID};
+      matching_engine_->sendMarketUpdate(&market_update_);
+
+      START_MEASURE(Exchange_UnorderedMapMEOrderBook_removeOrder);
+      removeOrder(order);
+      END_MEASURE(Exchange_UnorderedMapMEOrderBook_removeOrder, (*logger_));
+    } else {
+      market_update_ = {MarketUpdateType::MODIFY, order->market_order_id_, ticker_id, order->side_,
+                        order->price_, order->qty_, order->priority_};
+      matching_engine_->sendMarketUpdate(&market_update_);
+    }
+  }
+
+  /// Check if a new order with the provided attributes would match against existing passive orders on the other side of the order book.
+  /// This will call the match() method to perform the match if there is a match to be made and return the quantity remaining if any on this new order.
+  auto UnorderedMapMEOrderBook::checkForMatch(ClientId client_id, OrderId client_order_id, TickerId ticker_id, Side side, Price price, Qty qty, Qty new_market_order_id) noexcept {
+    auto leaves_qty = qty;
+
+    if (side == Side::BUY) {
+      while (leaves_qty && asks_by_price_) {
+        const auto ask_itr = asks_by_price_->first_me_order_;
+        if (LIKELY(price < ask_itr->price_)) {
+          break;
+        }
+
+        START_MEASURE(Exchange_UnorderedMapMEOrderBook_match);
+        match(ticker_id, client_id, side, client_order_id, new_market_order_id, ask_itr, &leaves_qty);
+        END_MEASURE(Exchange_UnorderedMapMEOrderBook_match, (*logger_));
+      }
+    }
+    if (side == Side::SELL) {
+      while (leaves_qty && bids_by_price_) {
+        const auto bid_itr = bids_by_price_->first_me_order_;
+        if (LIKELY(price > bid_itr->price_)) {
+          break;
+        }
+
+        START_MEASURE(Exchange_UnorderedMapMEOrderBook_match);
+        match(ticker_id, client_id, side, client_order_id, new_market_order_id, bid_itr, &leaves_qty);
+        END_MEASURE(Exchange_UnorderedMapMEOrderBook_match, (*logger_));
+      }
+    }
+
+    return leaves_qty;
+  }
+
+  /// Create and add a new order in the order book with provided attributes.
+  /// It will check to see if this new order matches an existing passive order with opposite side, and perform the matching if that is the case.
+  auto UnorderedMapMEOrderBook::add(ClientId client_id, OrderId client_order_id, TickerId ticker_id, Side side, Price price, Qty qty) noexcept -> void {
+    const auto new_market_order_id = generateNewMarketOrderId();
+    client_response_ = {ClientResponseType::ACCEPTED, client_id, ticker_id, client_order_id, new_market_order_id, side, price, 0, qty};
+    matching_engine_->sendClientResponse(&client_response_);
+
+    START_MEASURE(Exchange_UnorderedMapMEOrderBook_checkForMatch);
+    const auto leaves_qty = checkForMatch(client_id, client_order_id, ticker_id, side, price, qty, new_market_order_id);
+    END_MEASURE(Exchange_UnorderedMapMEOrderBook_checkForMatch, (*logger_));
+
+    if (LIKELY(leaves_qty)) {
+      const auto priority = getNextPriority(price);
+
+      auto order = order_pool_.allocate(ticker_id, client_id, client_order_id, new_market_order_id, side, price, leaves_qty, priority, nullptr,
+                                        nullptr);
+      START_MEASURE(Exchange_UnorderedMapMEOrderBook_addOrder);
+      addOrder(order);
+      END_MEASURE(Exchange_UnorderedMapMEOrderBook_addOrder, (*logger_));
+
+      market_update_ = {MarketUpdateType::ADD, new_market_order_id, ticker_id, side, price, leaves_qty, priority};
+      matching_engine_->sendMarketUpdate(&market_update_);
+    }
+  }
+
+  /// Attempt to cancel an order in the order book, issue a cancel-rejection if order does not exist.
+  auto UnorderedMapMEOrderBook::cancel(ClientId client_id, OrderId order_id, TickerId ticker_id) noexcept -> void {
+    auto is_cancelable = (client_id < cid_oid_to_order_.size());
+    MEOrder *exchange_order = nullptr;
+    if (LIKELY(is_cancelable)) {
+      auto &co_itr = cid_oid_to_order_[client_id];
+      exchange_order = co_itr[order_id];
+      is_cancelable = (exchange_order != nullptr);
+    }
+
+    if (UNLIKELY(!is_cancelable)) {
+      client_response_ = {ClientResponseType::CANCEL_REJECTED, client_id, ticker_id, order_id, OrderId_INVALID,
+                          Side::INVALID, Price_INVALID, Qty_INVALID, Qty_INVALID};
+    } else {
+      client_response_ = {ClientResponseType::CANCELED, client_id, ticker_id, order_id, exchange_order->market_order_id_,
+                          exchange_order->side_, exchange_order->price_, Qty_INVALID, exchange_order->qty_};
+      market_update_ = {MarketUpdateType::CANCEL, exchange_order->market_order_id_, ticker_id, exchange_order->side_, exchange_order->price_, 0,
+                        exchange_order->priority_};
+
+      START_MEASURE(Exchange_UnorderedMapMEOrderBook_removeOrder);
+      removeOrder(exchange_order);
+      END_MEASURE(Exchange_UnorderedMapMEOrderBook_removeOrder, (*logger_));
+
+      matching_engine_->sendMarketUpdate(&market_update_);
+    }
+
+    matching_engine_->sendClientResponse(&client_response_);
+  }
+
+  auto UnorderedMapMEOrderBook::toString(bool detailed, bool validity_check) const -> std::string {
+    std::stringstream ss;
+    std::string time_str;
+
+    auto printer = [&](std::stringstream &ss, MEOrdersAtPrice *itr, Side side, Price &last_price, bool sanity_check) {
+      char buf[4096];
+      Qty qty = 0;
+      size_t num_orders = 0;
+
+      for (auto o_itr = itr->first_me_order_;; o_itr = o_itr->next_order_) {
+        qty += o_itr->qty_;
+        ++num_orders;
+        if (o_itr->next_order_ == itr->first_me_order_)
+          break;
+      }
+      sprintf(buf, " <px:%3s p:%3s n:%3s> %-3s @ %-5s(%-4s)",
+              priceToString(itr->price_).c_str(), priceToString(itr->prev_entry_->price_).c_str(), priceToString(itr->next_entry_->price_).c_str(),
+              priceToString(itr->price_).c_str(), qtyToString(qty).c_str(), std::to_string(num_orders).c_str());
+      ss << buf;
+      for (auto o_itr = itr->first_me_order_;; o_itr = o_itr->next_order_) {
+        if (detailed) {
+          sprintf(buf, "[oid:%s q:%s p:%s n:%s] ",
+                  orderIdToString(o_itr->market_order_id_).c_str(), qtyToString(o_itr->qty_).c_str(),
+                  orderIdToString(o_itr->prev_order_ ? o_itr->prev_order_->market_order_id_ : OrderId_INVALID).c_str(),
+                  orderIdToString(o_itr->next_order_ ? o_itr->next_order_->market_order_id_ : OrderId_INVALID).c_str());
+          ss << buf;
+        }
+        if (o_itr->next_order_ == itr->first_me_order_)
+          break;
+      }
+
+      ss << std::endl;
+
+      if (sanity_check) {
+        if ((side == Side::SELL && last_price >= itr->price_) || (side == Side::BUY && last_price <= itr->price_)) {
+          FATAL("Bids/Asks not sorted by ascending/descending prices last:" + priceToString(last_price) + " itr:" + itr->toString());
+        }
+        last_price = itr->price_;
+      }
+    };
+
+    ss << "Ticker:" << tickerIdToString(ticker_id_) << std::endl;
+    {
+      auto ask_itr = asks_by_price_;
+      auto last_ask_price = std::numeric_limits<Price>::min();
+      for (size_t count = 0; ask_itr; ++count) {
+        ss << "ASKS L:" << count << " => ";
+        auto next_ask_itr = (ask_itr->next_entry_ == asks_by_price_ ? nullptr : ask_itr->next_entry_);
+        printer(ss, ask_itr, Side::SELL, last_ask_price, validity_check);
+        ask_itr = next_ask_itr;
+      }
+    }
+
+    ss << std::endl << "                          X" << std::endl << std::endl;
+
+    {
+      auto bid_itr = bids_by_price_;
+      auto last_bid_price = std::numeric_limits<Price>::max();
+      for (size_t count = 0; bid_itr; ++count) {
+        ss << "BIDS L:" << count << " => ";
+        auto next_bid_itr = (bid_itr->next_entry_ == bids_by_price_ ? nullptr : bid_itr->next_entry_);
+        printer(ss, bid_itr, Side::BUY, last_bid_price, validity_check);
+        bid_itr = next_bid_itr;
+      }
+    }
+
+    return ss.str();
+  }
+}
