abdb21f4e64f0afcc619baa06ffffd617bebd1f8
Sourav Ghosh <sghoshusc@gmail.com>
2023-08-12 17:45:36 -0500

    simplifications/improvements for socket code.

--- Chapter12/common/tcp_server.h
+++ Chapter12/common/tcp_server.h
@@ -11,8 +11,6 @@ namespace Common {
     /// Start listening for connections on the provided interface and port.
     auto listen(const std::string &iface, int port) -> void;
 
-    auto destroy();
-
     /// Check for new connections or dead connections and update containers that track the sockets.
     auto poll() noexcept -> void;
 
@@ -21,26 +19,22 @@ namespace Common {
 
   private:
     /// Add and remove socket file descriptors to and from the EPOLL list.
-    auto epoll_add(TCPSocket *socket);
-
-    auto epoll_del(TCPSocket *socket);
-
-    auto del(TCPSocket *socket);
+    auto addToEpollList(TCPSocket *socket);
 
   public:
     /// Socket on which this server is listening for new connections on.
-    int efd_ = -1;
+    int epoll_fd_ = -1;
     TCPSocket listener_socket_;
 
     epoll_event events_[1024];
 
     /// Collection of all sockets, sockets for incoming data, sockets for outgoing data and dead connections.
-    std::vector<TCPSocket *> sockets_, receive_sockets_, send_sockets_, disconnected_sockets_;
+    std::vector<TCPSocket *> receive_sockets_, send_sockets_;
 
     /// Function wrapper to call back when data is available.
-    std::function<void(TCPSocket *s, Nanos rx_time)> recv_callback_;
+    std::function<void(TCPSocket *s, Nanos rx_time)> recv_callback_ = nullptr;
     /// Function wrapper to call back when all data across all TCPSockets has been read and dispatched this round.
-    std::function<void()> recv_finished_callback_;
+    std::function<void()> recv_finished_callback_ = nullptr;
 
     std::string time_str_;
     Logger &logger_;
